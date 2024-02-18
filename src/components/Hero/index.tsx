dff328056410c3d63f0677c9ff77a1a5ca0098eb
alvesxdani <dani.alvesm22@gmail.com>
2024-02-18 17:11:46 -0300

    :recycle: refactor: colocando styled components nos componentes

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { StyledHero } from './style'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <StyledHero>
     <div className="container">
-      <h2 className={styles.heroTitle}>
+      <h2 className="heroTitle">
         As melhores vagas para tecnologia, design e artes visuais.
       </h2>
     </div>
-  </form>
+  </StyledHero>
 )
 
 export default Hero
